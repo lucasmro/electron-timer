5bb1dfa65b25fd800cb8085c6a196945f029f82c
Lucas Michelini Reis de Oliveira <lucasmro@gmail.com>
2018-09-28 20:51:54 -0300

    Add BrowserWindow and load URL

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    mainWindow.loadURL("http://www.google.com");
+});
