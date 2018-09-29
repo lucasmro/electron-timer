daf23db4b72fa647e7c903cd2ea49bdb50ad477d
Lucas Michelini Reis de Oliveira <lucasmro@gmail.com>
2018-09-29 08:21:29 -0300

    Add html template, fonts and images

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -2,6 +2,6 @@ const { ipcRenderer } = require('electron');
 
 let linkAbout = document.querySelector('#link-about');
 
-linkAbout.addEventListener('click', function() {
+linkAbout.addEventListener('click' , function() {
     ipcRenderer.send('open-about-window');
 });
